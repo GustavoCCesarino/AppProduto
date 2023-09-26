f1a3ad2a193fefd6c5a6cd031a783f66737b4205
Gustavo Cesarino <gccesarino220@gmail.com>
2023-09-26 19:18:36 -0300

    class produto + push pelo AndrStudio

---
+++ app/src/main/java/com/cesarino/appproduto/Produto.java
@@ -0,0 +1,22 @@
+package com.cesarino.appproduto;
+
+public class Produto {
+    public int id;
+    public String nome;
+    public double preco;
+
+    public Produto() {
+
+    }
+
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
